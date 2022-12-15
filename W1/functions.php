d53eecd83956841cb65fcea331e554eb6767b614
joegiesing <jrgiesing@email.neit.edu>
2022-12-15 07:47:59 -0500

    w1 and w2

---
+++ W1/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+//data dump
+function dd($data){
+
+    echo '<pre>';
+
+    die(var_dump($data));
+
+    echo '</pre>';
+    
+}
+
+//age of entry
+function entry($age){
+    if ($age >= 21) {
+        echo 'Come on in!';
+    }
+    else {
+        echo 'You are not old enough!';
+    }
+}
+
+//fizz uzz
+function fizzBuzz($num) 
+{
+    if ($num % 2 == 0 && $num % 3 == 0){
+        echo $num, ' Fizz Buzz<br>';
+    }
+    elseif ($num % 2 == 0) {
+        echo $num, ' Fizz<br>';
+    }
+    elseif ($num % 3 == 0) {
+        echo $num, ' Buzz<br>';
+    }
+}
+
+?>
---
+++ W1/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'functions.php';
+
+for ($i=0; $i <= 100; $i++) {
+    fizzBuzz($i);
+}
+
+require 'index.view.php';
